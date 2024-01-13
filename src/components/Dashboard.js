1856cce16137799ce90d17605b90ac4bf83fbbb3
Yordanos gebreselasie <yordibege@gmail.com>
2024-01-13 14:13:57 -0700

    Add the panel and loading components

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,55 @@
 import React, { Component } from "react";
-
 import classnames from "classnames";
+import Panel from "./Panel"; 
+import Loading from "./Loading"; 
+
+
+const data = [
+  {
+    id: 1,
+    label: "Total Photos",
+    value: 10
+  },
+  {
+    id: 2,
+    label: "Total Topics",
+    value: 4
+  },
+  {
+    id: 3,
+    label: "User with the most uploads",
+    value: "Allison Saeng"
+  },
+  {
+    id: 4,
+    label: "User with the least uploads",
+    value: "Lukas Souza" // Remove the extra double-quote here
+  }
+];
+
 
 class Dashboard extends Component {
+  state = {
+    loading: false
+  };
+
   render() {
     const dashboardClasses = classnames("dashboard");
 
-    return <main className={dashboardClasses} />;
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    const panels = data.map(panel => (
+      <Panel
+        key={panel.id}
+        id={panel.id}
+        label={panel.label}
+        value={panel.value}
+      />
+    ));
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
-
 export default Dashboard;
